9ab522d8e6dd23f96637d0a5e2f5974e8412eaee
Mout Pessemier <mout.pessemier@inthepocket.com>
2025-08-17 15:31:57 +0200

    feat: add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,59 @@
+import { clsx } from "clsx";
+import Link from "next/link";
+import { type ComponentPropsWithoutRef } from "react";
+
+import styles from "./card.module.css";
+
+type CardProps = ComponentPropsWithoutRef<"div">;
+
+export const Root = ({ children, className, ...props }: CardProps) => {
+  return (
+    <div className={clsx(styles.card, className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+type CardHeaderProps = ComponentPropsWithoutRef<"div">;
+
+export const Header = ({ children, ...props }: CardHeaderProps) => {
+  return (
+    <div className={styles["card-header"]} {...props}>
+      {children}
+    </div>
+  );
+};
+
+type CardContentProps = ComponentPropsWithoutRef<"div">;
+
+export const Content = ({
+  children,
+  className,
+  ...props
+}: CardContentProps) => {
+  return (
+    <div className={clsx(styles["card-content"], className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+type CardFooterProps = ComponentPropsWithoutRef<"div">;
+
+export const Footer = ({ children, className, ...props }: CardFooterProps) => {
+  return (
+    <div className={clsx(styles["card-footer"], className)} {...props}>
+      {children}
+    </div>
+  );
+};
+
+type CardAction = ComponentPropsWithoutRef<typeof Link>;
+
+export const Action = ({ children, className, ...props }: CardAction) => {
+  return (
+    <Link className={clsx(styles["card-action"], className)} {...props}>
+      {children}
+    </Link>
+  );
+};
